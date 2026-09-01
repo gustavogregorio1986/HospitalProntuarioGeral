463c8bee7a05b1669010349776070b00fe2ad9c3
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-09-01 11:21:43 -0300

    Tive que recriar

--- HospitalProntuario.Web/Models/ErrorViewModel.cs
+++ HospitalProntuario.Web/Models/ErrorViewModel.cs
@@ -4,6 +4,6 @@ namespace HospitalProntuario.Web.Models
     {
         public string? RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);   
     }
 }
