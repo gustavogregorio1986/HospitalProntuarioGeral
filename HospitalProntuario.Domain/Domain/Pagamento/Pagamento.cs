eb9e14a13ff1e55fb9954d6eadc33436767fbaee
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-09-08 13:15:47 -0300

    Desenvolver o o campo StatusPagamento no projeto Hospital Prontuario

--- HospitalProntuario.Domain/Domain/Pagamento/Pagamento.cs
+++ HospitalProntuario.Domain/Domain/Pagamento/Pagamento.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HospitalProntuario.Domain.Domain;
 
 namespace HospitalProntuario.Domain.Domain
 {
@@ -12,6 +13,9 @@ namespace HospitalProntuario.Domain.Domain
         public decimal Valor { get; set; }
         public DateTime Data { get; set; }
         public string FormaPagamento { get; set; }
+
+        // Status do pagamento (Pendente, Confirmado, Cancelado)
+        public StatusPagamento Status { get; set; }
 
         // Relacionamento
         public int PacienteId { get; set; }
---
+++ HospitalProntuario.Infra/Migrations/20260908161458_IncluirCampoStatusPagamwentoEnum.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalProntuario.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncluirCampoStatusPagamwentoEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "tb_Pagamento",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "tb_Pagamento");
+        }
+    }
+}
