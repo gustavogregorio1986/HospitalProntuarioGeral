4ecb4aaf8a8456b99209a636919ee08fce853a36
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-09-09 18:23:28 -0300

    Desenvolvendo as melhorias no projeto HospitalProntuario

--- HospitalProntuario.Domain/Services/IAgendamentoService.cs
+++ HospitalProntuario.Domain/Services/IAgendamentoService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HospitalProntuario.Domain.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace HospitalProntuario.Domain.Services
 {
     public interface IAgendamentoService
     {
+        Task<Agendamento> GetByIdAsync(int id);
+        Task<IEnumerable<Agendamento>> GetAllAsync();
+        Task AddAsync(Agendamento agendamento);
+        Task UpdateAsync(Agendamento agendamento);
+        Task DeleteAsync(int id);
     }
 }
--- HospitalProntuario.Services/Service/AgendamentoService.cs
+++ HospitalProntuario.Services/Service/AgendamentoService.cs
@@ -1,13 +1,50 @@
-﻿using HospitalProntuario.Domain.Services;
+﻿using HospitalProntuario.Domain.Domain;
+using HospitalProntuario.Domain.Repositories.Interface;
+using HospitalProntuario.Domain.Services;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace HospitalProntuario.Infra.Service
 {
     public class AgendamentoService : IAgendamentoService
     {
+        private readonly IAgendamentoRepository _agendamentoRepository;
+
+        public AgendamentoService(IAgendamentoRepository agendamentoRepository)
+        {
+            _agendamentoRepository = agendamentoRepository;
+        }
+
+        public async Task<Agendamento> GetByIdAsync(int id) =>
+            await _agendamentoRepository.GetByIdAsync(id);
+
+        public async Task<IEnumerable<Agendamento>> GetAllAsync() =>
+            await _agendamentoRepository.GetAllAsync();
+
+        public async Task AddAsync(Agendamento agendamento)
+        {
+            if (agendamento.Data < DateTime.Now)
+                throw new ArgumentException("A data do agendamento não pode ser no passado.");
+
+            await _agendamentoRepository.AddAsync(agendamento);
+            await _agendamentoRepository.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Agendamento agendamento)
+        {
+            _agendamentoRepository.Update(agendamento);
+            await _agendamentoRepository.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var agendamento = await _agendamentoRepository.GetByIdAsync(id);
+            if (agendamento == null)
+                throw new KeyNotFoundException("Agendamento não encontrado.");
+
+            _agendamentoRepository.Delete(agendamento);
+            await _agendamentoRepository.SaveChangesAsync();
+        }
     }
 }
