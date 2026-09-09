916582dc25a15e765e327f5a615324ab440c5da0
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-09-09 14:11:48 -0300

    Desenvolvendo os formularios, nessr commit desenvolvi o formulario de cores para funcionar no sistema interio

---
+++ HospitalProntuario.Web/Controllers/ConfiguracaoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalProntuario.Web.Controllers
+{
+    public class ConfiguracaoController : Controller
+    {
+        public IActionResult ConfiguracaoPadrao()
+        {
+            return View();
+        }
+    }
+}
