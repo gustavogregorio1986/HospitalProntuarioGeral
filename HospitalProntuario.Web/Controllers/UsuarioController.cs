ce6561784a90ef09bb3f55b20f59a3a87b8ffbac
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-09-09 10:27:52 -0300

    Desenvolver o menu de adminsitrador eusuario e recepcionista no projeto HospitalProntuario

---
+++ HospitalProntuario.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalProntuario.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult ListarUsuarios()
+        {
+            return View();
+        }
+
+        public IActionResult ListarDeRecepcionistas()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroDeUsuario()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroDeRecepcionistas()
+        {
+            return View();
+        }
+    }
+}
