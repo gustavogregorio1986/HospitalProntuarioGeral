46649b99c7cddcfe633e0de5739f4505ab0434d2
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-09-09 13:21:52 -0300

    Desenvolvendo os links e sublinkis no projeto do HospitalProntuario

---
+++ HospitalProntuario.Web/Controllers/PagamentoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalProntuario.Web.Controllers
+{
+    public class PagamentoController : Controller
+    {
+        public IActionResult ListarDePagamentos()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroDePagamento()
+        {
+            return View();
+        }
+    }
+}
+
