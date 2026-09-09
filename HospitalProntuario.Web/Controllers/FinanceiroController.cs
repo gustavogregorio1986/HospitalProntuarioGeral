cd7718d2a0b9f5b23ff25877a9e544640fed84e8
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-09-09 13:40:16 -0300

    Desenvolvendo os links paar as respecitivas paginas no projeto Hospital Prontuario

---
+++ HospitalProntuario.Web/Controllers/FinanceiroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalProntuario.Web.Controllers
+{
+    public class FinanceiroController : Controller
+    {
+        public IActionResult PagamentoFinanceiro()
+        {
+            return View();
+        }
+    }
+}
---
+++ HospitalProntuario.Web/Controllers/RelatorioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalProntuario.Web.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        public IActionResult RelatorioGeral()
+        {
+            return View();
+        }
+    }
+}
